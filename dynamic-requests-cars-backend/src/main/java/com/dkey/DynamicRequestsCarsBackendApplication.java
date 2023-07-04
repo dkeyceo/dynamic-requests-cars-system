06b6ff50563269df48dcc9d8025ae93b990a598d
dkeyceo <dkeyceo@gmail.com>
2023-07-04 17:57:48 +0300

    Addded backend skeleton, deps and created tree of packages

---
+++ dynamic-requests-cars-backend/src/main/java/com/dkey/DynamicRequestsCarsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicRequestsCarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicRequestsCarsBackendApplication.class, args);
+	}
+
+}
---
+++ dynamic-requests-cars-backend/src/test/java/com/dkey/DynamicRequestsCarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicRequestsCarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
